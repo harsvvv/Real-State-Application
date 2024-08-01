d1e0fa9de476955bf6fa8118d33c40bfea31a437
harsvvv <168051136+harsvvv@users.noreply.github.com>
2024-08-01 19:01:46 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
         '/api':{
-          target: 'http://localhost:3000',
+          target: 'https://techtronics-y2b2.onrender.com',
           secure:false
     }
   }
